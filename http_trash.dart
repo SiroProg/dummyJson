f21c54006ed4810089dd0bc23d110adda49d52a4
SiroProg <070777sss@gmail.com>
2023-08-08 09:53:33 +0500

    get one todo

--- http_trash.dart
+++ http_trash.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart';
 
 void main(List<String> args) async {
   String baseUrl = 'https://dummyjson.com/todos';
-  Uri uri = Uri.parse('$baseUrl');
+  Uri uri = Uri.parse('$baseUrl/3');
 
   Response responseGet = await get(uri);
 
