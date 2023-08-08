0c444f1b4b50431ff73dd36caea297f5315e1804
SiroProg <070777sss@gmail.com>
2023-08-08 09:54:08 +0500

    Get a random todo

--- http_trash.dart
+++ http_trash.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart';
 
 void main(List<String> args) async {
   String baseUrl = 'https://dummyjson.com/todos';
-  Uri uri = Uri.parse('$baseUrl/3');
+  Uri uri = Uri.parse('$baseUrl/random');
 
   Response responseGet = await get(uri);
 
