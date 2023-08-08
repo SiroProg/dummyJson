aabfd19174b69b0e5846d909065b435e45ca1492
SiroProg <070777sss@gmail.com>
2023-08-08 09:55:56 +0500

    Get all todos by user id

--- http_trash.dart
+++ http_trash.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart';
 
 void main(List<String> args) async {
   String baseUrl = 'https://dummyjson.com/todos';
-  Uri uri = Uri.parse('$baseUrl?limit=3&skip=10');
+  Uri uri = Uri.parse('$baseUrl/user/5');
 
   Response responseGet = await get(uri);
 
