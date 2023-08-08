bacaeca87d8cdcad50cfae3a80759a95140162d4
SiroProg <070777sss@gmail.com>
2023-08-08 09:55:02 +0500

    Limit and skip todos

--- http_trash.dart
+++ http_trash.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart';
 
 void main(List<String> args) async {
   String baseUrl = 'https://dummyjson.com/todos';
-  Uri uri = Uri.parse('$baseUrl/random');
+  Uri uri = Uri.parse('$baseUrl?limit=3&skip=10');
 
   Response responseGet = await get(uri);
 
