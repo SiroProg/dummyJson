7d9c33c2db2614f0967be3e22245b959b3cbd3a1
SiroProg <070777sss@gmail.com>
2023-08-08 10:27:20 +0500

    Delete a todo

--- http_trash.dart
+++ http_trash.dart
@@ -1,18 +1,10 @@
-import 'dart:convert';
-
 import 'package:http/http.dart';
-
-import 'models/todo_model.dart';
 
 void main(List<String> args) async {
   String baseUrl = 'https://dummyjson.com/todos';
   Uri uri = Uri.parse('$baseUrl/1');
 
-  Response responsePut = await put(
-    uri,
-    headers: <String, String>{'Content-Type': 'application/json'},
-    body: jsonEncode({'todo': 'New todo'}),
-  );
+  Response responseDelete = await delete(uri);
 
-  print(responsePut.body);
+  print(responseDelete.body);
 }
